f41305fe2fe326f454d951c8bfeb8f3442b2d15d
stella <stellavir11@gmail.com>
2022-09-02 12:02:51 +0300

    shell command url path settings

--- online_banking/perseverance/urls.py
+++ online_banking/perseverance/urls.py
@@ -6,4 +6,7 @@ app_name = 'perseverance'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/balance/', views.transaction, name='balance'),
+    path('<int:pk>/account/', views.AccountView.as_view(), name='account'),
 ]
