e1b306cbd5af1dbbffdd636bd978bf0cd21ec510
stella <stellavir11@gmail.com>
2022-09-02 12:35:12 +0300

    redo path

--- online_banking/perseverance/urls.py
+++ online_banking/perseverance/urls.py
@@ -6,4 +6,7 @@ app_name = 'perseverance'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/balance/', views.transaction, name='balance'),
+    path('<int:pk>/account/', vies.AccountView, name='account'),
 ]
